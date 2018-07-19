39d8744428121bbbe61857ab08f99ce3e6319b7d
Tibo Rigole <tibo.rigole@gmail.com>
2018-07-19 16:44:47 +0200

    enkel nog L en R richtingen, daarna nog de matrixgrenzen checken

--- src/Blokje.java
+++ src/Blokje.java
@@ -5,6 +5,7 @@ public class Blokje {
 	private int yco;
 	private int zco;
 	private char richting;
+	private char waarde;
 	
 	//constructoren
 	public Blokje() {
@@ -14,30 +15,86 @@ public class Blokje {
 		richting = 'O';
 	}
 	
+	public Blokje(int xco, int yco, int zco, char richting, char waarde) {
+		this.xco = xco;
+		this.yco = yco;
+		this.zco = zco;
+		this.richting= richting;
+		this.waarde = waarde;
+	}
+	
+	//setters
 	public void setXco(int x) {xco = x;}
 	public void setYco(int y) {yco = y;}
 	public void setZco(int z) {zco = z;}
 	public void setRichting(char r) {richting = r;}
-
+	public void setWaarde(char w) {waarde = w;}
+	
+	public void setAlles(int x, int y, int z, char richting, char waarde) {
+		xco = x;
+		yco = y;
+		zco = z;
+		this.richting = richting;
+		this.waarde = waarde;
+	}
+	
+	//getters
 	public int getXco() {return xco;}
 	public int getYco() {return yco;}
 	public int getZco() {return zco;}
 	public char getRichting() {return richting;}
-	
-	
-	public Blokje(int xco, int yco, int zco) {
-		this.xco = xco;
-		this.yco = yco;
-		this.zco = zco;
-	}
+	public char getWaarde() {return waarde;}
 
 	/**
 	 * zet de xco, yco, en zco op 0
+	 * zet de characters naar de beginorientatie en het stationwaarde
 	 */
 	public void reset() {
 		xco = 0;
 		yco = 0;
 		zco = 0;
+		richting = 'O';
+		waarde = 'S';
+	}
+	
+	// adhv de huidige richting kijken welke waarde +-1 (x of z)
+	public void setXenZco(Blokje vorigBlokje) {
+		
+		// oost ? x+1 , y ongewijzigd
+		if(vorigBlokje.getRichting()=='O') {
+			this.setXco(vorigBlokje.getXco()+1);
+			this.setZco(vorigBlokje.getZco());
+		}
+		
+		//west ? x-1 , y ongewijzigd
+		else if(vorigBlokje.getRichting()=='W') {
+			this.setXco(vorigBlokje.getXco()-1);
+			this.setZco(vorigBlokje.getZco());
+		}
+		
+		//noord ? z-1, x ongewijzigd
+		else if(vorigBlokje.getRichting()=='N') {
+			this.setXco(vorigBlokje.getXco());
+			this.setZco(vorigBlokje.getZco()-1);
+		}
+		
+		//zuid ? z+1, x ongewijzigd
+		else if(vorigBlokje.getRichting()=='Z') {
+			this.setXco(vorigBlokje.getXco());
+			this.setZco(vorigBlokje.getZco()+1);
+		}
+		
+	}
+
+	public void genereerYcoAdhvVorigBlokje(Blokje vorigBlokje) {
+		if(vorigBlokje.getWaarde()=='U') {
+			this.setYco(vorigBlokje.getYco()+1);
+		}
+		
+		else {
+			this.setYco(vorigBlokje.getYco());	
+		}
+		
 	}
 
 	
--- src/Main.java
+++ src/Main.java
@@ -13,30 +13,31 @@ public class Main {
 		String spoorSequentie = null;
 		char huidigeRichting = 'O';
 		char waarde = 'S';
+		
 		Blokje vorigBlokje = new Blokje();
 		Blokje volgendBlokje = new Blokje();
 		
-		
 		int [][] matrix = new int [5][5];
 		
+		//voor elke achtbaan
 		for(int achtbaanId=1 ; achtbaanId<aantalAchtbanen+1 ; achtbaanId++) {
-			
-			//variabelen resetten op beginwaarden
+		
+		//variabelen resetten op beginwaarden
 			vorigBlokje.reset();
 			volgendBlokje.reset();
 			huidigeRichting = 'O';
 			waarde = 'S';
-			matrix = new int [5][5];
+			matrix = new int [5][5];	//later nog naar 1,1 wijzigen zodat elke achtbaan er volledig opstaat
 			
-			//input lezen
+		//input lezen
 			lengteSpoor = sc.nextInt();
 			spoorSequentie = sc.nextLine();
 			
-			//de eerste is een spatie, deze moet nog weg
+		//de eerste is een spatie, deze moet nog weg
 			spoorSequentie = spoorSequentie.substring(1, spoorSequentie.length());
 			
 			
-			//overlopen van de achtbaan
+		//overlopen van de achtbaan
 			for(int i=0 ; i<lengteSpoor; i++) {
 				
 				volgendBlokje = volgendBlokje(huidigeRichting,vorigBlokje,waarde);
@@ -69,70 +70,76 @@ public class Main {
 		
 		switch(waarde) {
 		case 'S':
+			//volgend blokje:
+				//richting is zowieso O
+				// waarde met eigen methode
+				//xco +1
+				//y en zco blijven
+			
 				returnBlokje.setXco(vorigBlokje.getXco() +1);
 				returnBlokje.setYco(vorigBlokje.getYco());
 				returnBlokje.setZco(vorigBlokje.getZco());
 				returnBlokje.setRichting('O');
+				returnBlokje.setWaarde('=');
 			break;
 			
-		case 'V':	//gewoon rechtdoor
-			if(huidigeRichting == 'O') {
-				returnBlokje.setXco(vorigBlokje.getXco() +1);
-				returnBlokje.setYco(vorigBlokje.getYco());
-				returnBlokje.setZco(vorigBlokje.getZco());
-				returnBlokje.setRichting('O');
-			}
-			else if(huidigeRichting == 'Z') {
-				returnBlokje.setXco(vorigBlokje.getXco() -1);
-				returnBlokje.setYco(vorigBlokje.getYco());
-				returnBlokje.setZco(vorigBlokje.getZco());
-				returnBlokje.setRichting('Z');
-			}
-			else if(huidigeRichting == 'N') {
-				returnBlokje.setXco(vorigBlokje.getXco());
-				returnBlokje.setYco(vorigBlokje.getYco());
-				returnBlokje.setZco(vorigBlokje.getZco()-1);
-				returnBlokje.setRichting('N');
-			}
-			else if(huidigeRichting == 'W') {
-				returnBlokje.setXco(vorigBlokje.getXco());
-				returnBlokje.setYco(vorigBlokje.getYco());
-				returnBlokje.setZco(vorigBlokje.getZco()+1);
-				returnBlokje.setRichting('W');
-			}
+		case 'V':	
+				//gewoon rechtdoor
+				//richting blijft deze van de vorige
+				//adhv de richting van de vorige, x +-1 OF z+-1
+			
+			// yco +1 als vorig blokje een omhoog was
+			//deze 3 nog bundelen in 1 verzamelmethode, we gebruiken ze namelijk 3x
+			
+			returnBlokje.genereerYcoAdhvVorigBlokje(vorigBlokje);			
+			returnBlokje.setXenZco(vorigBlokje);
+			returnBlokje.setRichting(vorigBlokje.getRichting());
+			//niet zeker als het nu wel is aangepast
+			
+			//x,y en zco zijn nu ingevuld
+			//richting is ook ingevuld
+			
+			//teken ervan bepalen
+			returnBlokje.setWaarde('_');
 			break;
 			
 		case 'L':
+			
 			break;
 			
 		case 'D':
+			//yco moet moet met 1 verlaagd worden
+			returnBlokje.setYco(vorigBlokje.getYco()-1);
+			returnBlokje.setXenZco(vorigBlokje);
+			returnBlokje.setRichting(vorigBlokje.getRichting());
+			//x,y en zco zijn nu ingevuld
+			//richting is ook ingevuld
+			
+			//teken ervan bepalen
+			if(returnBlokje.getRichting()=='N' || returnBlokje.getRichting()=='Z') {
+				returnBlokje.setWaarde('#');
+			}
+			else if(returnBlokje.getRichting()=='O') {returnBlokje.setWaarde('\\');}
+			else if(returnBlokje.getRichting()=='W') {returnBlokje.setWaarde('/');}
+			
 			break;
 			
 		case 'U':
-			if(huidigeRichting == 'O') {
-				returnBlokje.setXco(vorigBlokje.getXco() +1);
-				returnBlokje.setYco(vorigBlokje.getYco()+1);
-				returnBlokje.setZco(vorigBlokje.getZco());
-				returnBlokje.setRichting('O');
+			// yco moet nog niet verhoogd woren
+			returnBlokje.setYco(vorigBlokje.getYco());
+			returnBlokje.setXenZco(vorigBlokje);
+			returnBlokje.setRichting(vorigBlokje.getRichting());
+			//x,y en zco zijn nu ingevuld
+			//richting is ook ingevuld
+			
+			//teken ervan bepalen
+			if(returnBlokje.getRichting()=='N' || returnBlokje.getRichting()=='Z') {
+				returnBlokje.setWaarde('#');
 			}
-			else if(huidigeRichting == 'Z') {
-				returnBlokje.setXco(vorigBlokje.getXco() -1);
-				returnBlokje.setYco(vorigBlokje.getYco()+1);
-				returnBlokje.setZco(vorigBlokje.getZco());
-				returnBlokje.setRichting('Z');
-			}
-			else if(huidigeRichting == 'N') {
-				returnBlokje.setXco(vorigBlokje.getXco());
-				returnBlokje.setYco(vorigBlokje.getYco());
-				returnBlokje.setZco(vorigBlokje.getZco()-1);
-				returnBlokje.setRichting('N');
-			}
-			else if(huidigeRichting == 'W') {
-				returnBlokje.setXco(vorigBlokje.getXco());
-				returnBlokje.setYco(vorigBlokje.getYco());
-				returnBlokje.setZco(vorigBlokje.getZco()+1);
-				returnBlokje.setRichting('W');
-			}
+			else if(returnBlokje.getRichting()=='O') {returnBlokje.setWaarde('/');}
+			else if(returnBlokje.getRichting()=='W') {returnBlokje.setWaarde('\\');}
+			
+			//adhv de richting van het vorige blokje kan je weten welke waarden er verhoogd moeten worden
 			break;
 			
 		case 'R':
@@ -146,5 +153,13 @@ public class Main {
 		
 		return returnBlokje;
 	}
+
+	/**
+	 * als het vorig blokje een U was, dan is de Yco +1
+	 * anders is het dezelfde yco waarde als het vorige blokje
+	 * 
+	 * @param vorigBlokje,
+	 * @returnt de yco van het volgende blokje
+	 */
 
 }
