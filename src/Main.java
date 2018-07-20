6dad06fb5788a361896a0d83547efe7c72b468a4
Tibo Rigole <tibo.rigole@gmail.com>
2018-07-20 14:48:58 +0200

    richtingen ok

--- src/Main.java
+++ src/Main.java
@@ -48,15 +48,14 @@ public class Main {
 				
 				//voeg het blokje toe aan de arrayList
 				blokjesLijst.add(volgendBlokje);
-
 				
 				//opt einde
 				vorigBlokje = volgendBlokje;
 				//enkel van belang als het een up is
 				if (waarde == 'U') {vorigBlokje.setSpoorTeken('U');}
+
 				
 			}
-			
 			//nu zitten alle blokjes in de blokjeslijst, de blokjes hebben x, y z coordinaten en het nodige teken
 			
 			//grenzen zetten van de matrix, verschuiven van de blokjes
@@ -103,7 +102,7 @@ public class Main {
 			System.out.println("maxy: "+maxY);*/
 			
 			//arrayList sorteren op blokjes: degene met de laagste Zco eerst
-			Collections.sort(blokjesLijst);
+			Collections.sort(blokjesLijst);	//vergemakkelijkt het printen
 			
 			/*for(Blokje x: blokjesLijst) {x.print();}
 			
@@ -130,19 +129,12 @@ public class Main {
 				sb.append('\n');
 			}
 			
+		//zorgen dat de output pas verschijnt bij een enter
+		if(achtbaanId == aantalAchtbanen) {String a = sc.nextLine();}
 			
-			
-			
-		
-			
-
-			
-			
-			
-			
-			
-			
+		//1 achtbaan is afgewerkt
 		}
+		
 		System.out.println(sb.toString());
 		
 		
@@ -206,7 +198,7 @@ public class Main {
 			
 		case 'U':
 			// yco moet nog niet verhoogd woren
-			returnBlokje.setYco(vorigBlokje.getYco());
+			returnBlokje.genereerYcoAdhvVorigBlokje(vorigBlokje);
 			returnBlokje.setXenZco(vorigBlokje);
 			returnBlokje.setRichting(vorigBlokje.getRichting());
 			//x,y en zco zijn nu ingevuld
@@ -247,12 +239,147 @@ public class Main {
 		return returnBlokje;
 	}
 
-	/**
-	 * als het vorig blokje een U was, dan is de Yco +1
-	 * anders is het dezelfde yco waarde als het vorige blokje
-	 * 
-	 * @param vorigBlokje,
-	 * @returnt de yco van het volgende blokje
-	 */
-
 }
+
+class Blokje implements Comparable {
+	
+	private int xco;
+	private int yco;
+	private int zco;
+	private char richting;
+	private char waarde;
+	private char spoorTeken;
+	
+	//constructoren
+	public Blokje() {
+		xco=0;
+		yco=0;
+		zco=0;
+		richting = 'O';
+	}
+	
+	public Blokje(int xco, int yco, int zco, char richting, char waarde) {
+		this.xco = xco;
+		this.yco = yco;
+		this.zco = zco;
+		this.richting= richting;
+		this.waarde = waarde;
+	}
+	
+	//setters
+	public void setXco(int x) {xco = x;}
+	public void setYco(int y) {yco = y;}
+	public void setZco(int z) {zco = z;}
+	public void setRichting(char r) {richting = r;}
+	public void setWaarde(char w) {waarde = w;}
+	
+	public void setAlles(int x, int y, int z, char richting, char waarde) {
+		xco = x;
+		yco = y;
+		zco = z;
+		this.richting = richting;
+		this.waarde = waarde;
+	}
+	
+	//getters
+	public int getXco() {return xco;}
+	public int getYco() {return yco;}
+	public int getZco() {return zco;}
+	public char getRichting() {return richting;}
+	public char getWaarde() {return waarde;}
+	private char getSpoorTeken() {return spoorTeken;}
+
+
+	/**
+	 * zet de xco, yco, en zco op 0
+	 * zet de characters naar de beginorientatie en het stationwaarde
+	 */
+	public void reset() {
+		xco = 0;
+		yco = 0;
+		zco = 0;
+		richting = 'O';
+		waarde = 'S';
+	}
+	
+	// adhv de huidige richting kijken welke waarde +-1 (x of z)
+	public void setXenZco(Blokje vorigBlokje) {
+		
+		// oost ? x+1 , y ongewijzigd
+		if(vorigBlokje.getRichting()=='O') {
+			this.setXco(vorigBlokje.getXco()+1);
+			this.setZco(vorigBlokje.getZco());
+		}
+		
+		//west ? x-1 , y ongewijzigd
+		else if(vorigBlokje.getRichting()=='W') {
+			this.setXco(vorigBlokje.getXco()-1);
+			this.setZco(vorigBlokje.getZco());
+		}
+		
+		//noord ? z-1, x ongewijzigd
+		else if(vorigBlokje.getRichting()=='N') {
+			this.setXco(vorigBlokje.getXco());
+			this.setZco(vorigBlokje.getZco()-1);
+		}
+		
+		//zuid ? z+1, x ongewijzigd
+		else if(vorigBlokje.getRichting()=='Z') {
+			this.setXco(vorigBlokje.getXco());
+			this.setZco(vorigBlokje.getZco()+1);
+		}
+		
+	}
+
+	public void genereerYcoAdhvVorigBlokje(Blokje vorigBlokje) {
+		if(vorigBlokje.getSpoorTeken()=='U') {
+			this.setYco(vorigBlokje.getYco()+1);
+		}
+		
+		else {
+			this.setYco(vorigBlokje.getYco());	
+		}
+		
+	}
+	
+	public void setRichtingDraaiRechts(char vorigBlokjeRichting) {
+		if(vorigBlokjeRichting=='N') {this.setRichting('O');}
+		if(vorigBlokjeRichting=='O') {this.setRichting('Z');}
+		if(vorigBlokjeRichting=='Z') {this.setRichting('W');}
+		if(vorigBlokjeRichting=='W') {this.setRichting('N');}
+	}
+
+	public void setRichtingDraaiLinks(char vorigBlokjeRichting) {
+		if(vorigBlokjeRichting=='N') {this.setRichting('W');}
+		if(vorigBlokjeRichting=='O') {this.setRichting('N');}
+		if(vorigBlokjeRichting=='Z') {this.setRichting('O');}
+		if(vorigBlokjeRichting=='W') {this.setRichting('Z');}
+	}
+
+	public void print() {
+		System.out.println(this.xco +","+ this.yco +","+this.zco +" ;;;"+this.richting);
+		
+	}
+
+	public void setSpoorTeken(char c) {
+		this.spoorTeken = c;
+		
+	}
+
+	public void verschuif(int minX, int minY) {
+		this.xco = this.xco+minX;
+		this.yco = this.yco+minY;
+		
+	}
+	//sorteren: laagste z waarden eerst
+
+	@Override
+	public int compareTo(Object tweedeBlokje) {
+		
+		int compareage=((Blokje)tweedeBlokje).getZco();
+		return this.zco-compareage;
+	}
+	
+}
+
+
